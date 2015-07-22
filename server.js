f4b9202c8a87e35073351880d7f406d32d74c70d
brettimus <brbeut@gmail.com>
2015-07-22 08:11:58 -0700

    first pass at serving html and josn

--- server.js
+++ server.js
@@ -2,4 +2,28 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require("express");
+var fs = require("fs");
+
+var app = express();
 var jsonData = {count: 12, message: 'hey'};
+
+
+app.get("/", function(req, res) {
+    fs.readFile(__dirname + "/index.html", function(err, data) {
+        if (err) {
+            res.status(500);
+            res.send("500 - Internal Error");
+            console.error(err);
+            return;
+        }
+        // TODO - send index.html
+        res.send(data.toString());
+    });
+});
+
+app.get("/data", function(req, res) {
+    res.json(jsonData);
+});
+
+app.listen(3000);
